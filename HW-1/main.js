e4dee61ac2d3c54c03ac98f1491d2174c01366be
lega2010 <lega10@i.ua>
2024-10-17 21:39:57 +0300

    Start

---
+++ HW-1/main.js
@@ -0,0 +1,42 @@
+let age = 14;
+
+console.log(age);
+
+//
+
+let name = "Oleg";
+
+console.log(name);
+
+//
+
+let isStudent = true;
+
+console.log(isStudent);
+
+//
+
+let myString = "Тут могла бути ваша реклама😉"
+
+console.log(myString);
+
+//
+
+let myNumber = 5;
+myNumber = myNumber + 10;
+
+console.log(myNumber);
+
+//
+
+let myNull = null;
+
+console.log(null);
+
+//
+
+const userName = prompt("Введіть своє ім'я")
+
+console.log(userName);
+
+//
